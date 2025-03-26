e0ddb423854cf1643ae44f45a67558ac60c1f984
Lucas Ricciardi <lucasmr@bell.net>
2025-03-25 20:01:28 -0400

    Added splashscreen for boot

--- src/BoardMaster.py
+++ src/BoardMaster.py
@@ -8,13 +8,18 @@ A free chess analysis tool
 #TODO: Add qsetting for piece images folder
 
 import sys
-from PySide6.QtWidgets import QApplication
-from PySide6.QtGui import QScreen
+import os
+from PySide6.QtWidgets import QApplication, QSplashScreen
+from PySide6.QtGui import QScreen, QPixmap, Qt
 from main_window import BoardMaster
 
 if __name__ == "__main__":
     # QGuiApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
     app = QApplication(sys.argv)
+    pixmap = QPixmap("../img/king.png")
+    splash = QSplashScreen(pixmap, Qt.WindowStaysOnTopHint)
+    splash.show()
+    QApplication.processEvents()
     window = BoardMaster()
     srcSize = QScreen.availableGeometry(QApplication.primaryScreen())
     frmX = (srcSize.width() - window.width()) / 2
@@ -22,6 +27,7 @@ if __name__ == "__main__":
     window.move(frmX, frmY)
     if window.engine:
         window.show()
+        splash.finish(window)
         sys.exit(app.exec())
     else:
         sys.exit(1)
